e9c0bc03805fbd534651381d25e30f9574aee61f
cwarrior01 <cheatanchavan50@gmail.com>
2024-08-07 18:12:32 +0530

    new code added

---
+++ src/firstANdlastposition.java
@@ -0,0 +1,37 @@
+public class firstANdlastposition {
+    public static void main(String[] args) {
+
+    }
+    static int search1(int[] arr, int target ){
+
+        int[] ans = {-1, -1};
+        int start = search( arr, target , true);
+        int end = search(arr,target , false);
+        ans [0]=start;
+        ans [1]=end;
+    }
+    static int search(int[] arr, int target , boolean findStartIndex) {
+        int ans = -1;
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+            if (findStartIndex){
+                end = mid -1 ;
+            }else {
+                start = mid + 1 ;
+            }
+            }
+        }
+        return ans;
+
+    }
+}
