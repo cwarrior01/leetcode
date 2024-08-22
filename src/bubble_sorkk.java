b73424ae8d83be083afe5bfb1f25fc1c9ef65d56
cwarrior01 <cheatanchavan50@gmail.com>
2024-08-22 15:35:14 +0530

    new code added

--- src/bubble_sorkk.java
+++ src/bubble_sorkk.java
@@ -2,19 +2,25 @@ import java.util.Arrays;
 public class bubble_sorkk {
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 2, 5, 6,};
-       bubble(arr);
+        bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     static void bubble(int[] arr) {
+        boolean swap;
         for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i + 1; j < arr.length - i-1; j++) {
-                if (arr[i] > arr[j]) {
+            swap = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j-1] > arr[j]) {
                     int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swap =  true;
                 }
 
+            }
+            if (swap == false){
+                break;
             }
         }
     }
